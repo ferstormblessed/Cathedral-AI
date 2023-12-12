a8ad76fded2d78015158a7dce923a946de4031a1
Fernando Camacho <salvadorfernando2002@outlook.com>
2023-12-12 21:37:49 +0100

    empeze con la funcion para sacar el espacio dominado alrededor de la catedral

--- src/main/java/ai/RandomCleverAI.java
+++ src/main/java/ai/RandomCleverAI.java
@@ -10,8 +10,6 @@ import de.fhkiel.ki.cathedral.game.Color;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -29,7 +27,7 @@ public class RandomCleverAI implements Agent {
         List<Placement> placesNearCathedral = placeNearCathedral(game, highestScoreBuildings);
         List<Placement> placesNearOwnBuilding = placeNearOwnBuilding(game, highestScoreBuildings);
 
-        if(!placesNearCathedral.isEmpty()) {
+        if (!placesNearCathedral.isEmpty()) {
             System.out.println("in near cathedral");
             System.out.println("Size of posible placements: " + placesNearCathedral.size());
             return Optional.of(selectRandomPlacement(placesNearCathedral));
@@ -38,8 +36,7 @@ public class RandomCleverAI implements Agent {
             System.out.println("in near building");
             System.out.println("Size of posible placements: " + placesNearOwnBuilding.size());
             return Optional.of(selectRandomPlacement(placesNearOwnBuilding));
-        }
-        else if (!highestScoreBuildings.isEmpty()) {
+        } else if (!highestScoreBuildings.isEmpty()) {
             System.out.println("in highest building");
             System.out.println("Size of posible placements: " + highestScoreBuildings.size());
             return Optional.of(selectRandomPlacement(highestScoreBuildings));
@@ -59,23 +56,34 @@ public class RandomCleverAI implements Agent {
         return Optional.empty();
     }
 
-    //get all the posible placements of the playable buildings with the highest score value
+    // evaluate turn (aka SCORE function)
+    @Override
+    public String evaluateLastTurn(Game game) {
+        return String.valueOf(game.getCurrentPlayer());
+    }
+
+    public int Score(Placement placement) {
+        return 0;
+    }
+
+    // get all the posible placements of the playable buildings with the highest
+    // score value
     private List<Placement> getHighestScoreBuildings(Game game) {
         List<Building> placeableBuildings = game.getPlacableBuildings();
 
         int highestScore = placeableBuildings
-            .stream()
-            .mapToInt(Building::score)
-            .max()
-            .orElse(-1);
+                .stream()
+                .mapToInt(Building::score)
+                .max()
+                .orElse(-1);
 
-        return placeableBuildings 
-            .stream()
-            .filter(building -> building.score() == highestScore)
-            .map(building -> building.getPossiblePlacements(game))
-            .flatMap(Collection::stream)
-            .distinct()
-            .collect(Collectors.toList());
+        return placeableBuildings
+                .stream()
+                .filter(building -> building.score() == highestScore)
+                .map(building -> building.getPossiblePlacements(game))
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     private List<Placement> placeNearCathedral(Game game, List<Placement> placements) {
@@ -84,19 +92,18 @@ public class RandomCleverAI implements Agent {
 
         for (Placement placement : placements) {
             List<Position> silhouette = placement.building().silhouette(placement.direction())
-                .stream()
-                .map(position -> position.plus(placement.position()))
-                .filter(Position::isViable)
-                .distinct()
-                .collect(Collectors.toList());
-            
+                    .stream()
+                    .map(position -> position.plus(placement.position()))
+                    .filter(Position::isViable)
+                    .distinct()
+                    .collect(Collectors.toList());
+
             Position currentPosition = placement.position();
             boolean isTouching = isTouchingCathedral(game, currentPosition);
 
             boolean isNearCathedral = silhouette
-                .stream()
-                .anyMatch(position -> Color.Blue == game.getBoard().getField()[position.y()][position.x()]);
-
+                    .stream()
+                    .anyMatch(position -> Color.Blue == game.getBoard().getField()[position.y()][position.x()]);
 
             if (isTouching) {
                 placesTouchingCathedral.add(placement);
@@ -116,13 +123,13 @@ public class RandomCleverAI implements Agent {
 
     private boolean isTouchingCathedral(Game game, Position pos) {
         Color[][] field = game.getBoard().getField();
-        if (pos.plus(1, 0).isViable() && field[pos.y()][pos.x() + 1] == Color.Blue)  {
+        if (pos.plus(1, 0).isViable() && field[pos.y()][pos.x() + 1] == Color.Blue) {
             return true;
-        } else if (pos.minus(1, 0).isViable() && field[pos.y()][pos.x() - 1] == Color.Blue)  {
+        } else if (pos.minus(1, 0).isViable() && field[pos.y()][pos.x() - 1] == Color.Blue) {
             return true;
-        } else if (pos.plus(0, 1).isViable() && field[pos.y() + 1][pos.x()] == Color.Blue)  {
+        } else if (pos.plus(0, 1).isViable() && field[pos.y() + 1][pos.x()] == Color.Blue) {
             return true;
-        } else if (pos.minus(0, 1).isViable() && field[pos.y() - 1][pos.x()] == Color.Blue)  {
+        } else if (pos.minus(0, 1).isViable() && field[pos.y() - 1][pos.x()] == Color.Blue) {
             return true;
         }
         return false;
@@ -137,17 +144,19 @@ public class RandomCleverAI implements Agent {
 
         for (Placement placement : placements) {
             List<Position> silhouette = placement.building().silhouette(placement.direction())
-                .stream()
-                .map(position -> position.plus(placement.position()))
-                .filter(Position::isViable)
-                .distinct()
-                .collect(Collectors.toList());
+                    .stream()
+                    .map(position -> position.plus(placement.position()))
+                    .filter(Position::isViable)
+                    .distinct()
+                    .collect(Collectors.toList());
 
             boolean isNearOwn = silhouette.stream()
-                .anyMatch(position -> placement.building().getColor() == game.getBoard().getField()[position.y()][position.x()]);
+                    .anyMatch(position -> placement.building()
+                            .getColor() == game.getBoard().getField()[position.y()][position.x()]);
 
             boolean isNearAWall = silhouette.stream()
-                .anyMatch(position -> position.x() == 0 || position.x() == boardWidth - 1 || position.y() == 0 || position.y() == boardHeight - 1);
+                    .anyMatch(position -> position.x() == 0 || position.x() == boardWidth - 1 || position.y() == 0
+                            || position.y() == boardHeight - 1);
 
             if (isNearAWall && isNearOwn) {
                 nearWallAndOwn.add(placement);
@@ -167,13 +176,44 @@ public class RandomCleverAI implements Agent {
         }
     }
 
+    private int AreaControlledAroundCathedral(Game game) {
+        Color myColor = game.getCurrentPlayer();
+        Integer colorID = myColor.getId();
+
+        if (colorID == 2) {
+            // i'm black
+            // search for id --> 2(color black) and 3(owned by black)
+        } else {
+            // i'm white
+            // search for id --> 4(color white) and 5(owned by white)
+        }
+
+        return 0;
+    }
+
+    // this helper function returns all the color id's of the tiles around the
+    // Cathedral
+    // / # # #
+    // # # C # #
+    // # C C C #
+    // # # C # #
+    // / # C #
+    // / # # #
+    private List<Integer> IDAroundCathedral(Game game) {
+        List<Integer> ids = new ArrayList<>();
+
+        // iterar por la 2D array de getField y sacar las posiciones que estan cerca de
+        // la catedral
+        return ids;
+    }
+
     private List<Placement> placeLastPieces(Game game) {
         return game.getPlacableBuildings()
-            .stream()
-            .map(building -> building.getPossiblePlacements(game))
-            .flatMap(Collection::stream)
-            .distinct()
-            .collect(Collectors.toList());
+                .stream()
+                .map(building -> building.getPossiblePlacements(game))
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
 
     }
 
@@ -181,4 +221,3 @@ public class RandomCleverAI implements Agent {
         return placements.get(new Random().nextInt(placements.size()));
     }
 }
-
